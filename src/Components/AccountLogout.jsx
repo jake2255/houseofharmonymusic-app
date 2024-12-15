74876297d69edf235634f773263faf122f55af4e
Ulloa, Jacob <jacobulloa@my.unt.edu>
2024-12-15 13:52:21 -0600

    modify logout csrf retrieval

--- src/Components/AccountLogout.jsx
+++ src/Components/AccountLogout.jsx
@@ -7,18 +7,35 @@ function AccountLogout()
 {
     const navigate = useNavigate();
 
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
     useEffect(() => {
         const logout = async () => {
             try {
-                const csrftoken = Cookies.get("csrftoken");
+                //const csrftoken = Cookies.get("csrftoken");
+                console.log("CSRF token before logout:", getCookie('csrftoken'));
+                console.log("CSRF token before logout:", Cookies.get("csrftoken"));
 
-                console.log("CSRF token before logout:", csrftoken);
                 
                 const logoutResponse = await axios.post(
                     'https://houseofharmonymusic-api.onrender.com/logout/', 
                     {},
                     {
-                        headers: { 'X-CSRFToken': csrftoken },
+                        headers: { 'X-CSRFToken': getCookie('csrftoken') },
                         withCredentials: true,
                     },
                 );
